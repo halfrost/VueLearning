f0a5b74543c166ffc81b8f7604950e2bc32c6a16
YDZ <ydz627@gmail.com>
2017-04-18 21:19:56 +0800

    v-for、v-bind

--- awesomeVue/src/App.vue
+++ awesomeVue/src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
+    <h1>{{title}}</h1>
     <img src="./assets/logo.png">
-    <router-view></router-view>
+    <ul>
+        <li v-for = "item in items" v-bind:class = "{finished: item.isFinished}"> {{item.label}} </li>
+    </ul>
   </div>
 </template>
 
+
+
 <script>
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      title: 'Welcome to My First Vue.js App',
+      msg: 'Hello world',
+      items: [
+        {
+          label: 'coding',
+          isFinished: false
+        },
+        {
+          label: 'walking',
+          isFinished: true
+        },
+        {
+          label: 'reading',
+          isFinished: false
+        }
+      ],
+      liClass: 'this is LiClass'
+    }
+  }
 }
+
 </script>
 
+
+
+
+
 <style>
+
+.finished {
+  text-decoration: underline;
+
+
+
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
