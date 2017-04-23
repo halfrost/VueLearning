fc4d6a1a6c81278a07a2490766e10d9cf0172e12
YDZ <ydz627@gmail.com>
2017-04-23 08:45:54 +0800

    add element-ui

--- awesomeVue/src/main.js
+++ awesomeVue/src/main.js
@@ -2,14 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  render: h => h(App)
 })
