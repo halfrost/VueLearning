1f98fe44f9027e88489249fa69e6a14cc4f7207d
YDZ <ydz627@gmail.com>
2017-04-24 23:12:09 +0800

    date-picker

--- awesomeVue/src/App.vue
+++ awesomeVue/src/App.vue
@@ -88,6 +88,30 @@
 <el-button>取消</el-button>
 </el-form-item>
 </el-form>
+
+
+<div class="block">
+  <span class="demonstration">默认</span>
+  <el-date-picker
+    v-model="value1"
+    type="datetime"
+    placeholder="选择日期时间">
+  </el-date-picker>
+</div>
+<div class="block">
+  <span class="demonstration">带快捷选项</span>
+  <el-date-picker
+    v-model="value2"
+    type="datetime"
+    placeholder="选择日期时间"
+    align="right"
+    :picker-options="pickerOptions1">
+  </el-date-picker>
+</div>
+
+
+
+
   </div>
 </template>
 
@@ -109,6 +133,30 @@ export default {
         desc: ''
       }
     }
+    pickerOptions1: {
+          shortcuts: [{
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        value1: '',
+        value2: ''
   },
   methods: {
     onSubmit () {
