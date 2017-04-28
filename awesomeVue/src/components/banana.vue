295c557ec7736f9d7f3aaf5d10ac4f4704abddcf
YDZ <ydz627@gmail.com>
2017-04-28 08:27:59 +0800

    Vue-Router

---
+++ awesomeVue/src/components/banana.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'banana',
+  data () {
+    return {
+      msg: 'I am banana'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- awesomeVue/src/router/index.js
+++ awesomeVue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Apple from '@/components/apple'
+import Banana from '@/components/banana'
 
 Vue.use(Router)
 
@@ -10,6 +12,14 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/apple',
+      component: Apple
+    },
+    {
+      path: '/banana',
+      component: Banana
     }
   ]
 })
