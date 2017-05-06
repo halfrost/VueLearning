38993455460433053995f836e6628ef9fe4c756b
YDZ <ydz627@gmail.com>
2017-05-06 22:20:18 +0800

    add fontclass / symbol / unicode font load

--- awesomeVue/src/pages/index.vue
+++ awesomeVue/src/pages/index.vue
@@ -1,9 +1,228 @@
-<template>
+  <template>
 <div class="book-content">
 
   <div id="__wrapper">
     <main class="container">
       <p class="dim text-center page-header">欢迎您 ydz627@gmail.com，您可以下载已购内容或进行新的购买</p>
+
+      <p>以下是symbol 加载方式</p>
+      <div class="row">
+      <ul class="icon_lists clear">
+                  <svg class="icon" aria-hidden="true" width = 30px height = 30px>
+                      <use xlink:href="#icon-pinterest"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-ios"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-dribbblecircled"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-qzone"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontgoogle0"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-and"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontdouban"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontfacebook"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontgoogle"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontinstagram"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfonttwitter"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontv2ex"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontwechat"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontweibo"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontwikipedia"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontzhihu"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-github"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-jianshu"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontyouku"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-youtube"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-luolei"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-evernotec"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-path78"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontqq"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-iconfontandroidwechat"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-gmail"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-pocket"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-line"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-JS"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-socialangular"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-dingding"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-wechat_moment_F"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-ziyuan"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-baidu"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-download_react-native"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-vuejs"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-element"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-juejin"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-Combined-Shape"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-element1"></use>
+                  </svg>
+                  <svg class="icon" aria-hidden="true">
+                      <use xlink:href="#icon-Webpack"></use>
+                  </svg>
+      </ul>
+    </div>
+
+      <p>以下是fontclass 加载方式</p>
+      <ul class="icon_lists clear">
+              <i class="icon iconfont icon-pinterest"></i>
+              <i class="icon iconfont icon-ios"></i>
+              <i class="icon iconfont icon-dribbblecircled"></i>
+              <i class="icon iconfont icon-qzone"></i>
+              <i class="icon iconfont icon-iconfontgoogle0"></i>
+              <i class="icon iconfont icon-and"></i>
+              <i class="icon iconfont icon-iconfontdouban"></i>
+              <i class="icon iconfont icon-iconfontfacebook"></i>
+              <i class="icon iconfont icon-iconfontgoogle"></i>
+              <i class="icon iconfont icon-iconfontinstagram"></i>
+              <i class="icon iconfont icon-iconfonttwitter"></i>
+              <i class="icon iconfont icon-iconfontv2ex"></i>
+              <i class="icon iconfont icon-iconfontwechat"></i>
+              <i class="icon iconfont icon-iconfontweibo"></i>
+              <i class="icon iconfont icon-iconfontwikipedia"></i>
+              <i class="icon iconfont icon-iconfontzhihu"></i>
+              <i class="icon iconfont icon-github"></i>
+              <i class="icon iconfont icon-jianshu"></i>
+              <i class="icon iconfont icon-iconfontyouku"></i>
+              <i class="icon iconfont icon-youtube"></i>
+              <i class="icon iconfont icon-luolei"></i>
+              <i class="icon iconfont icon-evernotec"></i>
+              <i class="icon iconfont icon-path78"></i>
+              <i class="icon iconfont icon-iconfontqq"></i>
+              <i class="icon iconfont icon-iconfontandroidwechat"></i>
+              <i class="icon iconfont icon-gmail"></i>
+              <i class="icon iconfont icon-pocket"></i>
+              <i class="icon iconfont icon-line"></i>
+              <i class="icon iconfont icon-JS"></i>
+              <i class="icon iconfont icon-socialangular"></i>
+              <i class="icon iconfont icon-dingding"></i>
+              <i class="icon iconfont icon-wechat_moment_F"></i>
+              <i class="icon iconfont icon-ziyuan"></i>
+              <i class="icon iconfont icon-baidu"></i>
+              <i class="icon iconfont icon-download_react-native"></i>
+              <i class="icon iconfont icon-vuejs"></i>
+              <i class="icon iconfont icon-element"></i>
+              <i class="icon iconfont icon-juejin"></i>
+              <i class="icon iconfont icon-Combined-Shape"></i>
+              <i class="icon iconfont icon-element1"></i>
+              <i class="icon iconfont icon-Webpack"></i>
+      </ul>
+
+      <br>以下是unicode 加载方式</br>
+      <ul class="icon_lists clear">
+                <i class="icon iconfont">&#xe82d;</i>
+                <i class="icon iconfont">&#xe658;</i>
+                <i class="icon iconfont">&#xe6a9;</i>
+                <i class="icon iconfont">&#xe605;</i>
+                <i class="icon iconfont">&#xe600;</i>
+                <i class="icon iconfont">&#xe61f;</i>
+                <i class="icon iconfont">&#xe636;</i>
+                <i class="icon iconfont">&#xe637;</i>
+                <i class="icon iconfont">&#xe638;</i>
+                <i class="icon iconfont">&#xe639;</i>
+                <i class="icon iconfont">&#xe63a;</i>
+                <i class="icon iconfont">&#xe63b;</i>
+                <i class="icon iconfont">&#xe63c;</i>
+                <i class="icon iconfont">&#xe63d;</i>
+                <i class="icon iconfont">&#xe63e;</i>
+                <i class="icon iconfont">&#xe63f;</i>
+                <i class="icon iconfont">&#xe640;</i>
+                <i class="icon iconfont">&#xe644;</i>
+                <i class="icon iconfont">&#xe645;</i>
+                <i class="icon iconfont">&#xe647;</i>
+                <i class="icon iconfont">&#xe649;</i>
+                <i class="icon iconfont">&#xe634;</i>
+                <i class="icon iconfont">&#xe646;</i>
+                <i class="icon iconfont">&#xe666;</i>
+                <i class="icon iconfont">&#xe641;</i>
+                <i class="icon iconfont">&#xf0095;</i>
+                <i class="icon iconfont">&#xf0099;</i>
+                <i class="icon iconfont">&#xf009b;</i>
+                <i class="icon iconfont">&#xe635;</i>
+                <i class="icon iconfont">&#xe501;</i>
+                <i class="icon iconfont">&#xe6f5;</i>
+                <i class="icon iconfont">&#xe61d;</i>
+                <i class="icon iconfont">&#xe630;</i>
+                <i class="icon iconfont">&#xe642;</i>
+                <i class="icon iconfont">&#xe65f;</i>
+                <i class="icon iconfont">&#xe617;</i>
+                <i class="icon iconfont">&#xe606;</i>
+                <i class="icon iconfont">&#xe601;</i>
+                <i class="icon iconfont">&#xe602;</i>
+                <i class="icon iconfont">&#xe60c;</i>
+                <i class="icon iconfont">&#xe643;</i>
+        </ul>
 
       <div class="row" >
           <book-card v-for="item in bookInfo" :key="item.id" :book-cover-image=item.bookCoverImage :book-price="item.bookPrice"></book-card>
@@ -49,6 +268,23 @@ export default {
 @import '../style/style.css';
 @import '../style/bootstrap-flat.min.css';
 
+@font-face {
+  font-family: 'iconfont';
+  src: url('../fonts/iconfont.eot');
+  src: url('../fonts/iconfont.eot?#iefix') format('embedded-opentype'),
+  url('../fonts/iconfont.woff') format('woff'),
+  url('../fonts/iconfont.ttf') format('truetype'),
+  url('../fonts/iconfont.svg#iconfont') format('svg');
+}
+
+.iconfont{
+  font-family:"iconfont" !important;
+  font-size:16px;font-style:normal;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;
+}
+
 .book-content {
   margin-top: 70px;
 }
@@ -82,4 +318,31 @@ export default {
 .clearfix:after {
   clear: both
 }
+
+.icon_lists{
+  width: 100% !important;
+
+}
+
+/*.icon_lists li{
+  float:left;
+  width: 100px;
+  height:180px;
+  text-align: center;
+  list-style: none !important;
+}*/
+.icon_lists .icon{
+  font-size: 42px;
+  line-height: 100px;
+  margin: 10px 0;
+  color:#333;
+  -webkit-transition: font-size 0.25s ease-out 0s;
+  -moz-transition: font-size 0.25s ease-out 0s;
+  transition: font-size 0.25s ease-out 0s;
+
+}
+/*这一行打开会让所有icon，鼠标移动到上面就触发这个hover，这里是放大效果*/
+/*.icon_lists .icon:hover{
+  font-size: 100px;
+}*/
 </style>
