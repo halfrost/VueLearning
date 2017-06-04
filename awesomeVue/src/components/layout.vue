3b2d6405f8e7fede7f9b0faea31577c23bbbeab0
YDZ <ydz627@gmail.com>
2017-06-04 08:35:31 +0800

    增加阴影线

--- awesomeVue/src/components/layout.vue
+++ awesomeVue/src/components/layout.vue
@@ -165,8 +165,7 @@ export default {
 
 .navbar {
   background-color: #f8f8f8;
-  border-color: #e7e7e7;
-  border-color: rgb(231, 231, 231);
+  border-bottom: 1px solid #e7e7e7;
 }
 
 .navbar-collapse {
