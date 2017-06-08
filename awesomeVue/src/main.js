55fa2f0ca5711779ccd1e4865a88e353b81f60ad
YDZ <ydz627@gmail.com>
2017-06-08 13:43:28 +0800

    去掉原来遗留的多余代码

--- awesomeVue/src/main.js
+++ awesomeVue/src/main.js
@@ -1,18 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import router from './router'
 import Layout from './components/layout'
-import BootstrapVue from 'bootstrap-vue'
+import * as uiv from 'uiv'
+// import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
+Vue.use(uiv)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+
 
 /* eslint-disable no-new */
 new Vue({
