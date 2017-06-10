f0b0dce4c675a1d15079bf639a4e71143e6d8815
YDZ <ydz627@gmail.com>
2017-06-11 01:50:41 +0800

    修复 navbar 选择状态的灰色底部显示

--- awesomeVue/src/components/navigationBar.vue
+++ awesomeVue/src/components/navigationBar.vue
@@ -2,33 +2,40 @@
 <div id="navigationbar">
   <nav class="navbar navbar-default">
 
-          <div class="container">
+    <div class="container">
 
-            <div class="navbar-header">
-              <!-- <div class="container"> -->
-              <button type="button" class="navbar-toggle collapsed" @click="showNavbar=!showNavbar">
+      <div class="navbar-header">
+        <!-- <div class="container"> -->
+        <button type="button" class="navbar-toggle collapsed" @click="showNavbar=!showNavbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" role="button">[@] ObjC 中国</a>
-            </div>
-            <collapse class="navbar-collapse text-align" v-model="showNavbar">
-              <ul class="nav navbar-nav">
-                <li ><a href="/issues/">期刊</a></li>
-                <li class="active"><a href="/products/">书籍</a></li>
-                <li ><a href="/issues/">购物车</a></li>
-              </ul>
+        <router-link to="/" class="navbar-brand" role="button" style="color: #777; background-color: transparent;">[@] ObjC 中国</router-link>
+      </div>
+      <collapse class="navbar-collapse text-align" v-model="showNavbar">
+        <ul class="nav navbar-nav">
+          <li>
+            <router-link to="/issues">期刊</router-link>
+          </li>
+          <li>
+            <router-link to="/products">书籍</router-link>
+          </li>
+          <li>
+            <router-link to="/goodscart">购物车</router-link>
+          </li>
+        </ul>
 
-              <ul class="nav navbar-nav navbar-right">
-                <li><a class="iconstyle"><iframe frameborder="0" height="20px" scrolling="0" src="https://ghbtns.com/github-btn.html?user=halfrost&amp;repo=Halfrost-Field&amp;type=star&amp;count=true&amp;" width="100px"></iframe></a></li>
-                <li><a href="/login/"><span class="glyphicon glyphicon-log-in"></span> 登录</a></li>
-              </ul>
-            </collapse>
-          </div>
-          <!-- </div> -->
-        </nav>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a class="iconstyle" style="padding-bottom:10px;color: #777; background-color: transparent;"><iframe frameborder="0" height="20px" scrolling="0" src="https://ghbtns.com/github-btn.html?user=halfrost&amp;repo=Halfrost-Field&amp;type=star&amp;count=true&amp;" width="100px"></iframe></a></li>
+          <li>
+            <router-link to="/login"><span class="glyphicon glyphicon-log-in"></span> 登录</router-link>
+          </li>
+        </ul>
+      </collapse>
+    </div>
+  </nav>
 </div>
 </template>
 
@@ -43,9 +50,25 @@ export default {
 }
 </script>
 
-<style scoped>
-.iconstyle{
+<style >
+.iconstyle {
   padding-bottom: 10px;
   margin-right: 0px;
+  color: #777;
+}
+
+.active-disable>.router-link-active {
+  color: #777;
+}
+
+.router-link-active {
+  color: #555;
+  background-color: #e7e7e7;
+}
+
+.navbar-default .navbar-nav>li>a:focus,
+.navbar-default .navbar-nav>li>a:hover {
+  color: #555;
+  background-color: #e7e7e7;
 }
 </style>
