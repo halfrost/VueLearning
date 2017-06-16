4ba17da6bab580a4ecadadf018c2c06137b9ab8c
YDZ <ydz627@gmail.com>
2017-06-16 15:04:29 +0800

    删除多余符号

--- awesomeVue/src/components/footerView.vue
+++ awesomeVue/src/components/footerView.vue
@@ -4,7 +4,7 @@
     <a href="https://onevcat.com">OneV's Den</a> &amp;
     <a href="http://objccn.io/">ObjC 中国</a>
     <br>本站由
-    <a href="https://halfrost.com'">@halfrost</a> 创建
+    <a href="https://halfrost.com">@halfrost</a> 创建
   </span>
 </div>
 </template>
