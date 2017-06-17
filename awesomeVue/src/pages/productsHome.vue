1afefe40fe8838a108d43bb7c2cc529a28dd719e
YDZ <ydz627@gmail.com>
2017-06-17 10:28:10 +0800

    书籍界面未登陆，跳转登陆界面

--- awesomeVue/src/pages/productsHome.vue
+++ awesomeVue/src/pages/productsHome.vue
@@ -2,7 +2,7 @@
 <div class="book-content">
   <div id="__wrapper">
     <main class="container">
-      <p class="dim text-center page-header">欢迎您 ydz627@gmail.com，您可以下载已购内容或进行新的购买</p>
+      <p class="dim text-center page-header">已经购买，需要重新下载？点击<router-link to="/login/">登录</router-link></p>
       <div class="row" >
           <book-card v-for="item in bookInfo" :key="item.id" :book-cover-image=item.bookCoverImage :book-price="item.bookPrice" :book-detail-info=item.bookDetailInfo></book-card>
       </div>
